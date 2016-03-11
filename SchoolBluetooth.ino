07c3e9af8d54ac7a586db1bbe8d224dc02049a4a
Ja Da <software@panduro.guru>
2016-03-11 12:29:12 +0100

    :tada:

--- SchoolBluetooth.ino
+++ SchoolBluetooth.ino
@@ -5,10 +5,17 @@ const int txPin = 9;
 const int rxPin = 8;
 
 // Pins to write to
-const int ledPin = 13;
+const int greenLed  = 11;
+const int yellowLed = 12;
+const int redLed    = 13;
 
 // Read value from Bluetooth
-int bluetoothValue;
+char bluetoothValue;
+
+// Toggle states
+int greenState  = 0;
+int yellowState = 0;
+int redState    = 0;
 
 // Define the bluetooth serial
 // (For connecting to the HC-05)
@@ -17,17 +24,39 @@ SoftwareSerial bluetoothSerial(rxPin, txPin);
 void setup() {
 
   // Debugging
-  Serial.begin(9600);
+  Serial.begin(38400);
+
+  while (!Serial) {
+    ;  
+  }
+
 
   // Define baud rate for the bluetooth connection
-  bluetoothSerial.begin(9600);
+  bluetoothSerial.begin(38400);
+  
+  pinMode(3, OUTPUT);
+  digitalWrite(3, HIGH);
 
-  pinMode(ledPin, OUTPUT);
+  bluetoothSerial.print("AT+NAME=BBBBB\r\n");
+  delay(1000);
+  while (bluetoothSerial.available()) {
+    Serial.write(bluetoothSerial.read());
+  }
 
+  // Pinmodes 
+  pinMode(greenLed, OUTPUT);
+  pinMode(yellowLed, OUTPUT);
+  pinMode(redLed, OUTPUT);
 }
 
 
 void loop() {
+
+  if (bluetoothSerial.available())
+    Serial.println(bluetoothSerial.readString());
+
+  if (Serial.available())
+    bluetoothSerial.println(Serial.readString());
 
   // Read from the BLuetooth module
   bluetoothValue = bluetoothSerial.read();
@@ -35,12 +64,12 @@ void loop() {
   // Print the Bluetooth value
   Serial.println(bluetoothValue);
  
-  if (bluetoothValue == 97) {
-    digitalWrite(ledPin, HIGH);
+  if (bluetoothValue == 'a') {
+    yellowState = !yellowState;
+    digitalWrite(yellowLed, yellowState);
   }
-  else {
-    digitalWrite(ledPin, LOW);
-  }
+
+  digitalWrite(yellowLed, HIGH);
 
   delay(500);
 }
