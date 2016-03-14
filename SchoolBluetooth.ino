e11d7d565885cfec20c4909b87570edbe4f6655a
Ja Da <software@panduro.guru>
2016-03-14 08:12:35 +0100

    It's not broken again
    Made it more scalable too :cat:

--- SchoolBluetooth.ino
+++ SchoolBluetooth.ino
@@ -1,21 +1,22 @@
 #include <SoftwareSerial.h>
 
 // Define the bluetooth ports
-const int txPin = 9;
-const int rxPin = 8;
+const int txPin = 6;
+const int rxPin = 5;
 
 // Pins to write to
 const int greenLed  = 11;
 const int yellowLed = 12;
 const int redLed    = 13;
 
-// Read value from Bluetooth
-char bluetoothValue;
+// Values for the leds
+const int greenValue  = 97; // = a
+const int yellowValue = 98; // = b 
+const int greenLed    = 99; // = c
 
-// Toggle states
-int greenState  = 0;
-int yellowState = 0;
-int redState    = 0;
+// Read value from Bluetooth
+int bluetoothValue;
+int previousBluetoothValue;
 
 // Define the bluetooth serial
 // (For connecting to the HC-05)
@@ -24,39 +25,17 @@ SoftwareSerial bluetoothSerial(rxPin, txPin);
 void setup() {
 
   // Debugging
-  Serial.begin(38400);
-
-  while (!Serial) {
-    ;  
-  }
-
+  Serial.begin(9600);
 
   // Define baud rate for the bluetooth connection
-  bluetoothSerial.begin(38400);
-  
-  pinMode(3, OUTPUT);
-  digitalWrite(3, HIGH);
+  bluetoothSerial.begin(9600);
 
-  bluetoothSerial.print("AT+NAME=BBBBB\r\n");
-  delay(1000);
-  while (bluetoothSerial.available()) {
-    Serial.write(bluetoothSerial.read());
-  }
-
-  // Pinmodes 
   pinMode(greenLed, OUTPUT);
-  pinMode(yellowLed, OUTPUT);
-  pinMode(redLed, OUTPUT);
+
 }
 
 
 void loop() {
-
-  if (bluetoothSerial.available())
-    Serial.println(bluetoothSerial.readString());
-
-  if (Serial.available())
-    bluetoothSerial.println(Serial.readString());
 
   // Read from the BLuetooth module
   bluetoothValue = bluetoothSerial.read();
@@ -64,13 +43,17 @@ void loop() {
   // Print the Bluetooth value
   Serial.println(bluetoothValue);
  
-  if (bluetoothValue == 'a') {
-    yellowState = !yellowState;
-    digitalWrite(yellowLed, yellowState);
-  }
+  sendLed(97, greenLed);
 
-  digitalWrite(yellowLed, HIGH);
-
-  delay(500);
+  // This is to make sure that the toggle isn't repeated
+  previousBluetoothValue = bluetoothValue;
+  
+  delay(100);
 }
 
+void sendLed(int ledValue, int led){
+  if (bluetoothValue == ledValue && bluetoothValue != previousBluetoothValue){
+      digitalWrite(led, !digitalRead(led);
+      delay(500);
+  }
+}
